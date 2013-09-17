1d76363007b9f5f5c47bcd9188088ff398b10888
vvallejo07 <bk_soft@hotmail.com>
2013-09-17 18:11:02 -0500

    segundo
    Segundo commit

--- Hotel-master/js/API/captura.js
+++
@@ -1,9 +0,0 @@
-//captura
-function tomarFoto(){
-	navigator.device.capture.captureImage(function(mediaFiles){
-		path = mediaFiles[0].fullPath;
-		$('#regFoto').append('<br><img src="'+path+'" width="100%" />').attr('rel',path);
-	},function(error){
-		navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-	},{limit:2});
-}
